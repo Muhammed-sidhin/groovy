ef0aea8b46fbb85248ef2013ca557ea336672519
Muhammed-sidhin <92983643+Muhammed-sidhin@users.noreply.github.com>
2022-11-30 10:43:53 +0530

    Create groovy.script

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp() {
+    echo 'build the application'
+}
+
+def testApp() {
+    echo 'testing the application'
+}
+
+def deployApp() {
+    echo 'deploying the application'
+    sh 'echo deploying version is ${VERSION}'
+}
